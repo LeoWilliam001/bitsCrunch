46cfb21738ce50a9c7e6b8a24dea263ebef184b0
Leo William <gleowilliam@gmail.com>
2025-01-30 21:37:29 +0530

    Update GlobalContext.js

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { createContext, useContext, useState } from "react";
-
+//Hello
 const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
